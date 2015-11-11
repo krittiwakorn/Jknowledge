6c12c966244a1875a16ded57b80be3d65a83392f
Chaiwat <mynameistecs51@gmail.com>
2015-11-11 19:53:52 +0700

    te.chaiwat update function add course success

--- application/controllers/Registration.php
+++ application/controllers/Registration.php
@@ -28,16 +28,35 @@
 		public function insert_course()
 		{
 			$this->form_validation->set_rules('course_code', 'course code', 'trim|required|xss_clean|numeric|max_length[11]');
-			$this->form_validation->set_rules('course_name', 'course name', 'trim|required|xss_clean|alpha');
+			$this->form_validation->set_rules('course_name', 'course name', 'trim|required|xss_clean|');
 			// ----set error message----//
 			$this->form_validation->set_message('numeric', 'กรุณากรอก %s เป็นตัวเลข 0-9');
 			$this->form_validation->set_message('required', 'กรุณากรอกข้อมูล %s.');
 
 			if($this->form_validation->run() == FALSE){	//form validation error
-				$this->load->view('course');
+				
+				$this->course();		//redirect function course
 				return FALSE;
 			}else{
 				$this->Mo_regis->insert_course();
+			}
+		}
+
+		public function update_course()
+		{
+			if($this->input->post('course_update') == 'course_update'){
+				$this->Mo_regis->update_course();
+			}else{
+				$this->course();
+			}
+		}
+
+		public function del_course($value='')		//del course
+		{	
+			if($value != null){
+				$this->Mo_regis->del_course($value);
+			}else{
+				$this->course();
 			}
 		}
 	}
--- application/models/Mo_regis.php
+++ application/models/Mo_regis.php
@@ -9,7 +9,7 @@ class Mo_regis extends CI_Model {
 		date_default_timezone_set('Asia/Bangkok');
 	}
 
-	function insert_course(){
+	function insert_course(){  // เพิ่มคอร์สเรียน
 		$course_code = $this->input->post('course_code');
 		$course_name = $this->input->post('course_name');
 		$status = $this->input->post('status');
@@ -31,11 +31,44 @@ class Mo_regis extends CI_Model {
 		redirect('Registration/course','refresh');
 	}
 
-	public function get_course()
+	public function get_course()  //แสดงคอร์ดเรียน
 	{
 		$getCourse = $this->db->get('course')->result();
 		return $getCourse;
 	}
+
+	public function update_course()
+	{
+		$course_id = $this->input->post('id_course');
+		$course_code = $this->input->post('course_code');
+		$course_name = $this->input->post('course_name');
+		$status = $this->input->post('status');
+		$comment = $this->input->post('comment');
+
+		$date = date("Y-m-d H:i:s");
+
+		$update_course = array(
+			'course_code' => $course_code,
+			'course_name' => $course_name,
+			'status' => $status,
+			'comment' => $comment,
+			'id_create' => '11111',
+			// 'dt_create' => $date,
+			'id_update' => '2222',
+			'dt_update' => $date,
+			);
+		$this->db->where('id_course',$course_id);
+		$this->db->update('course',$update_course);
+		redirect('Registration/course','refresh');
+	}
+
+	public function del_course($value='')
+	{
+		$this->db->where('course_code',$value);
+		$this->db->delete('course');
+		redirect('Registration/course','refresh');
+	}
+
 
 }
 
