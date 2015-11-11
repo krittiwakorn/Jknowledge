aeb2e030975a1301f9fc20477a6c719479da38b1
chaiwat <mynameistecs51@gmail.com>
2015-11-11 09:54:49 +0700

    te.chaiwat add databas jknowledge

--- application/views/header.php
+++ application/views/header.php
@@ -50,13 +50,13 @@
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav">
                     <li>
-                        <a href="#">About</a>
+                        <a href="#">เพิ่มคอร์ส</a>
                     </li>
                     <li>
-                        <a href="#">Services</a>
+                        <a href="#">เพิ่มแอดมิน</a>
                     </li>
                     <li>
-                        <a href="#">Contact</a>
+                        <a href="#">รายงาน</a>
                     </li>
                 </ul>
             </div>
---
+++ db_Jknowledge/jknowledge.sql
@@ -0,0 +1,61 @@
+-- phpMyAdmin SQL Dump
+-- version 4.4.14
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 11, 2015 at 03:49 AM
+-- Server version: 5.6.26
+-- PHP Version: 5.6.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `jknowledge`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `course`
+--
+
+CREATE TABLE IF NOT EXISTS `course` (
+  `course_id` int(11) NOT NULL,
+  `course_code` char(50) NOT NULL,
+  `course_name` varchar(100) NOT NULL,
+  `status` text NOT NULL,
+  `comment` text NOT NULL,
+  `id_create` int(11) NOT NULL,
+  `dt_create` datetime NOT NULL,
+  `id_update` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `course`
+--
+ALTER TABLE `course`
+  ADD PRIMARY KEY (`course_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `course`
+--
+ALTER TABLE `course`
+  MODIFY `course_id` int(11) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
